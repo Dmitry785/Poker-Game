2eb30aaf83818d72fe347d9a86297012f1ca03aa
Dmitry785 <danchenko.d2020@mail.ru>
2026-02-16 09:02:47 +0300

    add mainwindow viewmodel

---
+++ Poker/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Poker.ViewModels
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        public string CurrentPageName;
+        public UserControl CurrentPage;
+        public ICommand PageSelectedCommand;
+    }
+}
