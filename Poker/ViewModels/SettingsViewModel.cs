1afc96295163e9bfe94404ef7857a53267058d4b
Dmitry785 <danchenko.d2020@mail.ru>
2026-02-18 09:04:31 +0300

    add poker table

--- Poker/ViewModels/SettingsViewModel.cs
+++ Poker/ViewModels/SettingsViewModel.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,17 +25,29 @@ namespace Poker.ViewModels
                 OnPropertyChanged();
             }
         }
+        public string CurrentIP => _game.CurrentIP.ToString();
         public ICommand ConnectCommand{ get; }
+        public ICommand StartHostCommand { get; }
         public SettingsViewModel(GameService game, SignalBus sb)
         {
             _game = game;
             ConnectCommand = new Command(OnConnect);
+            StartHostCommand = new Command(OnStartHost);
             sb.Subscribe<StateChangedMessage>(HandleStatusChanged);
+            NetworkChange.NetworkAddressChanged += (s, e) =>
+            {
+                OnPropertyChanged(nameof(CurrentIP));
+            };
+        }
+        private void OnStartHost()
+        {
+            MessageBox.Show("Start host");
         }
         private void OnConnect()
         {
             if (!IPEndPoint.TryParse(HostIP, out var ip))
                 return;
+            MessageBox.Show(HostIP);
             _game.Connect(ip);
         }
         private void HandleStatusChanged(StateChangedMessage message)
